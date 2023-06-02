b93f416db0d95c7cf839c78b9aa5843bcc010899
Haruna Motosugi <haruna.motosugi@asterone.co.jp>
2023-06-02 14:11:36 +0900

    [add]コメントへのリプライ投稿

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ fetch("data.json")
     //   key: value,
     // }, list=[q1,2,3,3,32,42,3,2]
 
-    // comment1 > content
+
+    // comment1 > content(Amy)------------------
     const avatar = document.querySelector(".avatars");
     avatar.src = data.comments[0].user.image.webp;
 
@@ -54,15 +55,33 @@ fetch("data.json")
       }
     });
 
-    // coment1 > add-comment1
+    //----------------------------------------------------------
+
+    // coment1 > add-comment1-----------------------------------
     const avatarJ = document.querySelectorAll(".avatars2");
     avatarJ.forEach(function (avatar) {
       avatar.src = data.currentUser.image.webp;
     });
+    //----------------------------------------------------------
 
     //<li id="comment1">内のReplyボタン押下でadd-comment1出現
 
     let replybutton = document.querySelector("#comment1 .icon-reply");
+
+    const addReplyButton1 = document.createElement("button");
+    addReplyButton1.className = "button";
+    addReplyButton1.textContent = "REPLY";
+
+    const newReply = document.createElement("textarea");
+    newReply.name = "comment";
+    newReply.placeholder = "Add a comment...";
+    newReply.className = "comment";
+
+    const addReplyBox = document.createElement("div");
+    addReplyBox.className = "add-comment1";
+
+    const comment1 = document.querySelector("#comment1");
+
     
     replybutton.addEventListener(
       "click",
@@ -72,30 +91,172 @@ fetch("data.json")
         avatars2.alt = data.currentUser.username;
         avatars2.className = "avatars2";
 
-        const newComment = document.createElement("textarea");
-        newComment.name = "comment";
-        newComment.placeholder = "Add a comment...";
-        newComment.className = "comment";
 
-        const addCommentBox = document.createElement("div");
-        addCommentBox.className = "add-comment1";
 
-        const addReplyButton = document.createElement("button");
-        addReplyButton.className = "button";
-        addReplyButton.textContent = "REPLY";
 
-        addCommentBox.appendChild(avatars2);
-        addCommentBox.appendChild(newComment);
-        addCommentBox.appendChild(addReplyButton);
 
-        const commentList = document.querySelector("#comment1");
-        commentList.appendChild(addCommentBox);
+
+        addReplyBox.appendChild(avatars2);
+        addReplyBox.appendChild(newReply);
+        addReplyBox.appendChild(addReplyButton1);
+
+  
+        comment1.appendChild(addReplyBox);
 
       },
       { once: true }
     );
+    
+   //-----------------------------------------------------------
 
-    // comment2 > content
+   //Amyへのリプライ
+
+   addReplyButton1.addEventListener("click",function () {
+
+    console.log("hakutyann");
+
+    const content = document.createElement("div")
+    content.className = "content"
+
+    //スコアボタン
+    const scorenun = document.createElement("div")
+    scorenun.className = "scorenun"
+    scorenun.textContent = 0 //データは仮
+
+    const score = document.createElement("div")
+    score.className = "score"
+
+    const plusBtnImg = document.createElement("img")
+    plusBtnImg.src = "images/icon-plus.svg"
+    plusBtnImg.alt = "plus"
+  
+    const plusbutton = document.createElement("button")
+    plusbutton.className = "plusBtn"
+
+    plusbutton.appendChild(plusBtnImg)
+
+    const minusBtnImg = document.createElement("img")
+    minusBtnImg.src = "images/icon-minus.svg"
+    minusBtnImg.alt = "minus"
+
+    const minusbutton = document.createElement("button")
+    minusbutton.className = "minusBtn"
+
+    minusbutton.appendChild(minusBtnImg)
+
+    score.appendChild(plusbutton)
+    score.appendChild(scorenun)
+    score.appendChild(minusbutton)
+
+
+    //　user-headline
+
+      //user-name
+      const myAvatar = document.createElement("img");
+      myAvatar.src = data.currentUser.image.webp;
+      myAvatar.alt = data.currentUser.username;
+      myAvatar.className = "avatars";
+
+      const name = document.createElement("div")
+      name.textContent = data.currentUser.username
+      name.className = "name"
+
+      const you = document.createElement("div")
+      you.textContent = "you"
+      you.className = "you"
+
+      const date = document.createElement("div")
+      date.textContent = new Date();
+      date.className = "date"
+
+      
+
+      const userName = document.createElement("div")
+      userName.className = "user-name"
+
+      userName.appendChild(myAvatar)
+      userName.appendChild(name)
+      userName.appendChild(you)
+      userName.appendChild(date)
+
+    
+
+      // deleteicon
+      const deleteImg = document.createElement("img")
+      deleteImg.src = "images/icon-delete.svg"
+      deleteImg.alt = "delete"
+
+      const textDelete = document.createElement("span")
+      textDelete.textContent ="Delete"
+
+
+      const iconDelete = document.createElement("div")
+      iconDelete.className = "icon-delete"
+
+      iconDelete.appendChild(deleteImg)
+      iconDelete.appendChild(textDelete)
+
+      const iconDeleteBtn = document.createElement("button")
+      
+      iconDeleteBtn.appendChild(iconDelete)
+
+      // edit-icon
+
+      const editImg = document.createElement("img")
+      editImg.src = "images/icon-edit.svg"
+      editImg.alt = "edit"
+
+      const textEdit = document.createElement("span")
+      textEdit.textContent ="Edit"
+
+
+      const iconEdit = document.createElement("div")
+      iconEdit.className = "icon-edit"
+
+      iconEdit.appendChild(editImg)
+      iconEdit.appendChild(textEdit)
+
+      const iconEditBtn = document.createElement("button")
+      
+      iconEditBtn.appendChild(iconEdit)
+
+      const deleteeditIcon = document.createElement("div")
+      deleteeditIcon.className = "delete-edit-icon"
+
+      deleteeditIcon.appendChild(iconDelete)
+      deleteeditIcon.appendChild(iconEdit)
+
+      // text
+      const text = document.createElement("div")
+      text.textContent = newReply.value
+      text.className = "text"
+
+  
+    content.appendChild(score)
+    content.appendChild(userName)
+    content.appendChild(deleteeditIcon)
+    content.appendChild(text)
+
+
+
+    const repliesA = document.createElement("ul")
+    repliesA.className = "replies"
+    const replyA = document.createElement("li")
+    replyA.className = "reply1"
+
+
+  
+    replyA.appendChild(content)
+    repliesA.appendChild(replyA)
+    // addReplyBox.after(repliesA)
+    comment1.appendChild(repliesA)
+    // comments.appendChild(comment1)
+
+  })
+
+  //----------------------------------------------------
+
+    // comment2 > content(Max)
     const avatarM = document.querySelector("#comment2 .avatars");
     avatarM.src = data.comments[1].user.image.webp;
 
@@ -131,9 +292,22 @@ fetch("data.json")
       }
     });
 
+    //---------------------------------------------------
+
     //<li id="comment2">内のReplyボタン押下でadd-comment1出現
 
     replybutton = document.querySelector("#comment2 .icon-reply");
+
+    const comment2 = document.querySelector("#comment2");
+
+    const newReply2 = document.createElement("textarea");
+    newReply2.name = "comment";
+    newReply2.placeholder = "Add a comment...";
+    newReply2.className = "comment";
+
+    const addReplyButton2 = document.createElement("button");
+    addReplyButton2.className = "button";
+    addReplyButton2.textContent = "REPLY";
 
     replybutton.addEventListener(
       "click",
@@ -143,28 +317,174 @@ fetch("data.json")
         avatars2.alt = data.currentUser.username;
         avatars2.className = "avatars2";
 
-        const newComment = document.createElement("textarea");
-        newComment.name = "comment";
-        newComment.placeholder = "Add a comment...";
-        newComment.className = "comment";
 
-        const addCommentBox = document.createElement("div");
-        addCommentBox.className = "add-comment1";
 
-        const addReplyButton = document.createElement("button");
-        addReplyButton.className = "button";
-        addReplyButton.textContent = "REPLY";
+        const addReplyBox = document.createElement("div");
+        addReplyBox.className = "add-comment1";
 
-        addCommentBox.appendChild(avatars2);
-        addCommentBox.appendChild(newComment);
-        addCommentBox.appendChild(addReplyButton);
+
+
+        addReplyBox.appendChild(avatars2);
+        addReplyBox.appendChild(newReply2);
+        addReplyBox.appendChild(addReplyButton2);
 
         const replies = document.querySelector("#comment2 > ul");
 
-        replies.before(addCommentBox);
+        replies.before(addReplyBox);
       },
       { once: true }
     );
+
+    //----------------------------------------------------------
+
+    //Maxへのリプライ
+
+    addReplyButton2.addEventListener("click",function () {
+
+      console.log("hakutyann");
+  
+      const content = document.createElement("div")
+      content.className = "content"
+  
+      //スコアボタン
+      const scorenun = document.createElement("div")
+      scorenun.className = "scorenun"
+      scorenun.textContent = 0 //データは仮
+  
+      const score = document.createElement("div")
+      score.className = "score"
+  
+      const plusBtnImg = document.createElement("img")
+      plusBtnImg.src = "images/icon-plus.svg"
+      plusBtnImg.alt = "plus"
+    
+      const plusbutton = document.createElement("button")
+      plusbutton.className = "plusBtn"
+  
+      plusbutton.appendChild(plusBtnImg)
+  
+      const minusBtnImg = document.createElement("img")
+      minusBtnImg.src = "images/icon-minus.svg"
+      minusBtnImg.alt = "minus"
+  
+      const minusbutton = document.createElement("button")
+      minusbutton.className = "minusBtn"
+  
+      minusbutton.appendChild(minusBtnImg)
+  
+      score.appendChild(plusbutton)
+      score.appendChild(scorenun)
+      score.appendChild(minusbutton)
+  
+  
+      //　user-headline
+  
+        //user-name
+        const myAvatar = document.createElement("img");
+        myAvatar.src = data.currentUser.image.webp;
+        myAvatar.alt = data.currentUser.username;
+        myAvatar.className = "avatars";
+  
+        const name = document.createElement("div")
+        name.textContent = data.currentUser.username
+        name.className = "name"
+  
+        const you = document.createElement("div")
+        you.textContent = "you"
+        you.className = "you"
+  
+        const date = document.createElement("div")
+        date.textContent = new Date();
+        date.className = "date"
+  
+        
+  
+        const userName = document.createElement("div")
+        userName.className = "user-name"
+  
+        userName.appendChild(myAvatar)
+        userName.appendChild(name)
+        userName.appendChild(you)
+        userName.appendChild(date)
+  
+      
+  
+        // deleteicon
+        const deleteImg = document.createElement("img")
+        deleteImg.src = "images/icon-delete.svg"
+        deleteImg.alt = "delete"
+  
+        const textDelete = document.createElement("span")
+        textDelete.textContent ="Delete"
+  
+  
+        const iconDelete = document.createElement("div")
+        iconDelete.className = "icon-delete"
+  
+        iconDelete.appendChild(deleteImg)
+        iconDelete.appendChild(textDelete)
+  
+        const iconDeleteBtn = document.createElement("button")
+        
+        iconDeleteBtn.appendChild(iconDelete)
+  
+        // edit-icon
+  
+        const editImg = document.createElement("img")
+        editImg.src = "images/icon-edit.svg"
+        editImg.alt = "edit"
+  
+        const textEdit = document.createElement("span")
+        textEdit.textContent ="Edit"
+  
+  
+        const iconEdit = document.createElement("div")
+        iconEdit.className = "icon-edit"
+  
+        iconEdit.appendChild(editImg)
+        iconEdit.appendChild(textEdit)
+  
+        const iconEditBtn = document.createElement("button")
+        
+        iconEditBtn.appendChild(iconEdit)
+  
+        const deleteeditIcon = document.createElement("div")
+        deleteeditIcon.className = "delete-edit-icon"
+  
+        deleteeditIcon.appendChild(iconDelete)
+        deleteeditIcon.appendChild(iconEdit)
+  
+        // text
+        const text = document.createElement("div")
+        text.textContent = newReply2.value
+        text.className = "text"
+  
+    
+      content.appendChild(score)
+      content.appendChild(userName)
+      content.appendChild(deleteeditIcon)
+      content.appendChild(text)
+  
+  
+  
+      const repliesM = document.createElement("ul")
+      repliesM.className = "replies"
+      const replyM = document.createElement("li")
+      replyM.className = "reply1"
+  
+  
+    
+      replyM.appendChild(content)
+      repliesM.appendChild(replyM)
+
+      comment2.appendChild(repliesM)
+
+  
+    })
+
+
+
+    //----------------------------------------------------------
 
     // replies > reply1
     const avatarR = document.querySelector("#comment2 .reply1 .avatars");
@@ -209,6 +529,8 @@ fetch("data.json")
       }
     });
 
+    //-----------------------------------------------------------
+
     //<li class="reply1">内のReplyボタン押下でadd-comment1出現
 
     replybutton = document.querySelector("#comment2 > ul > li.reply1 > div > button > div")
@@ -243,6 +565,8 @@ fetch("data.json")
       },
       { once: true }
     );
+
+    //-------------------------------------------------------------
 
     // replies > edit-reply-content
 
@@ -286,6 +610,7 @@ fetch("data.json")
       }
     });
 
+  //-----------------------------------------------------------------
 
   // コメント追加ボックス
 
@@ -316,6 +641,7 @@ fetch("data.json")
 
   comments.after(addCommentBox);
 
+//----------------------------------------------------------
 
 // 投稿されたボックス
 // const scorenun = document.createElement("div");
@@ -446,16 +772,7 @@ fetch("data.json")
       text.textContent = newComment.value
       text.className = "text"
 
-      function checkText() {
-
-        if(text.length === 0) {
-            alert('文字が入力されていません！');
-            return false;
-        }
-      }
-
-
-
+    
 
 
 
@@ -480,6 +797,8 @@ fetch("data.json")
 
 
   })
+
+  //-------------------------------------------------
 
 
 
