01aa05c669d20348e72848121f5d76d170e0aeaf
Haruna Motosugi <haruna.motosugi@asterone.co.jp>
2023-06-23 11:40:30 +0900

    [fix]リファクタリング

--- helpers.js
+++ helpers.js
@@ -1,45 +1,14 @@
 export async function getFileData() {
-  /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function */
   const response = await fetch("data.json");
   return response.json();
 }
 
-function createCommentContent(score, username, src, date, text, isCurrentUser) {
-  const template = document.getElementById("content-template");
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/Node/cloneNode
-  const clone = template.content.cloneNode(true).firstElementChild;
-  // cloneの中身を書き換える
-  clone.querySelector("p.text").textContent = text;
-  clone.querySelector(".date").textContent = date;
-  clone.querySelector(".scorenun").textContent = score;
-  clone.querySelector("img.avatars").src = src;
-  clone.querySelector(".name").textContent = username;
-
-  if (isCurrentUser) {
-    clone.querySelector(".you").textContent = "you";
-    const icon_reply = clone.querySelector(".icon-reply");
-
-    // const editDelete = document.getElementById("edit-delete-template").content.cloneNode(true);
-    // reply           .parentElement.appendChild(editDelete);
-
-    const editDelete = createEditDelete();
-    icon_reply.parentElement.appendChild(editDelete);
-    icon_reply.remove();
-    // createEditDelete();
-  }
-
-  return clone;
-}
-
 export function processComment(comment, currentUser) {
-  const ul = document.getElementById("comments");
-
-  const li = document.createElement("li");
-  li.classList.add("comment");
+  const ul_comments = document.getElementById("comments");
+  const li_comment = document.createElement("li");
+  li_comment.classList.add("comment");
   const ul_replies = document.createElement("ul");
   ul_replies.classList.add("replies");
-  //  const li = document.getElementById("comment")
 
   const contentClone = createCommentContent(
     comment.score,
@@ -50,35 +19,76 @@ export function processComment(comment, currentUser) {
     comment.user.username === "juliusomo"
   );
 
-  // cloneを追加する
-  li.appendChild(contentClone);
-
-  ul.appendChild(li);
+  li_comment.appendChild(contentClone);
+  ul_comments.appendChild(li_comment);
 
   addScoreFunctionality(contentClone, comment.score);
 
-  addReplyFunctionality(contentClone, currentUser, li, ul_replies);
-
-  // const ul_replies = document.createElement("ul");
-  // ul_replies.classList.add("replies");
-
-  // const ul_replies =document.getElementById("replies")
-  // li.appendChild(ul_replies)
+  replyForm(contentClone, currentUser, li_comment);
 
   for (const reply of comment.replies) {
-    processReply(reply, ul_replies, currentUser, li);
-
-    // console.log("reply",reply);
-    // li.appendChild(ul_replies)
+    processReply(reply, ul_replies, currentUser, li_comment);
   }
-
-  // li.appendChild(ul_replies);
-
-  // ul_replies.appendChild(li_reply)
-  // ul.appendChild(li);
 }
 
-function addReplyFunctionality(contentClone, currentUser, li, li_reply) {
+export function processReply(comment, ul_replies, currentUser, li) {
+  const replyClone = createCommentContent(
+    comment.score,
+    comment.user.username,
+    comment.user.image.webp,
+    comment.createdAt,
+    comment.content,
+    comment.user.username === "juliusomo"
+  );
+
+  addScoreFunctionality(replyClone, comment.score);
+
+  const li_reply = document.createElement("li");
+  li_reply.classList.add("reply");
+  li_reply.appendChild(replyClone);
+
+  ul_replies.appendChild(li_reply);
+  li.appendChild(ul_replies);
+
+  const ta = document.createElement("textarea");
+  ta.classList.add("text");
+  ta.value = li_reply.querySelector("p").textContent;
+  ta.classList.add("hide");
+  replyClone.appendChild(ta);
+  replyForm(replyClone, currentUser, ul_replies);
+}
+
+export function commentForm(currentUser) {
+  const currentUserClone = currentUserInfoCmt(currentUser.image.webp);
+  addCommentFunctionality(currentUserClone, currentUser);
+  document
+    .getElementById("comments")
+    .insertAdjacentElement("afterend", currentUserClone.firstElementChild);
+}
+
+function createCommentContent(score, username, src, date, text, isCurrentUser) {
+  const template = document.getElementById("content-template");
+
+  const clone = template.content.cloneNode(true).firstElementChild;
+
+  clone.querySelector("p.text").textContent = text;
+  clone.querySelector(".date").textContent = date;
+  clone.querySelector(".scorenun").textContent = score;
+  clone.querySelector("img.avatars").src = src;
+  clone.querySelector(".name").textContent = username;
+
+  if (isCurrentUser) {
+    clone.querySelector(".you").textContent = "you";
+    const icon_reply = clone.querySelector(".icon-reply");
+    const editDelete = createEditDelete();
+    icon_reply.parentElement.appendChild(editDelete);
+    icon_reply.remove();
+  }
+
+  return clone;
+}
+
+function replyForm(contentClone, currentUser, li) {
   const replyButton = contentClone.querySelector(".icon-reply");
   if (!replyButton) return;
   replyButton.onclick = () => {
@@ -88,10 +98,10 @@ function addReplyFunctionality(contentClone, currentUser, li, li_reply) {
       return;
     }
 
-    const replyClone = createAddReply(currentUser.image.webp);
-    contentClone.insertAdjacentElement("afterend", replyClone);
+    const clone = CurrentUserInfoReply(currentUser.image.webp);
+    contentClone.insertAdjacentElement("afterend", clone);
 
-    addNewReply(li_reply, replyClone, currentUser);
+    addNewReplyFunctionality(clone, currentUser);
   };
 }
 
@@ -104,12 +114,10 @@ function addScoreFunctionality(clone, score) {
   minusButton.onclick = () => (scoreTag.textContent = score - 1);
 }
 
-function addCommentFunctionality(clone, currentUser, templateClone) {
+function addCommentFunctionality(clone, currentUser) {
   const sendButton = clone.querySelector(".button");
-  // console.log(sendButton);
 
   sendButton.onclick = (e) => {
-    // console.log("clicked");
     if (e.target.previousElementSibling.value === "") {
       e.disabled = true;
 
@@ -120,33 +128,30 @@ function addCommentFunctionality(clone, currentUser, templateClone) {
       0,
       currentUser.username,
       currentUser.image.webp,
-      date(/**/).textContent,
-      // templateClone.querySelector("textarea").value,
+      date().textContent,
       e.target.previousElementSibling.value,
       true
     );
 
-    addScoreFunctionality(commentData, 0); //0手打ち
+    addScoreFunctionality(commentData, 0);
 
+    const ta = document.createElement("textarea");
+    ta.classList.add("text");
+    ta.value = e.target.previousElementSibling.value;
+    ta.classList.add("hide");
 
-    const ta = document.createElement("textarea")
-    ta.classList.add("text")
-    ta.value = e.target.previousElementSibling.value
-    ta.classList.add("hide")
+    const ul_comments = document.getElementById("comments");
 
-    const ul = document.getElementById("comments");
-
-    const li = document.createElement("li");
-    li.classList.add("comment");
-    li.appendChild(commentData);
-    commentData.appendChild(ta)
-    ul.appendChild(li);
-    // console.log(li);
+    const li_comment = document.createElement("li");
+    li_comment.classList.add("comment");
+    li_comment.appendChild(commentData);
+    commentData.appendChild(ta);
+    ul_comments.appendChild(li_comment);
     e.target.previousElementSibling.value = "";
   };
 }
 
-function addNewReply(li_reply, replyClone, currentUser) {
+function addNewReplyFunctionality(replyClone, currentUser) {
   const replyButton = replyClone.querySelector("button");
   replyButton.onclick = (e) => {
     if (e.target.previousElementSibling.value === "") {
@@ -164,28 +169,23 @@ function addNewReply(li_reply, replyClone, currentUser) {
       true
     );
 
-    // console.log(replyClone.querySelector("textarea").value);
-    addScoreFunctionality(div_content, 0); //0手打ち？
-
-
+    addScoreFunctionality(div_content, 0);
 
     const li = document.createElement("li");
     li.classList.add("reply");
     li.appendChild(div_content);
 
-    const ta = document.createElement("textarea")
-    ta.classList.add("text")
-    ta.value = li.querySelector("p").textContent
-    ta.classList.add("hide")
+    const ta = document.createElement("textarea");
+    ta.classList.add("text");
+    ta.value = li.querySelector("p").textContent;
+    ta.classList.add("hide");
 
-    div_content.appendChild(ta)
+    div_content.appendChild(ta);
 
     const ul_rtr = document.createElement("ul");
     ul_rtr.classList.add("replies");
     ul_rtr.appendChild(li);
-    // console.log(e.target.closest(".li"));
     e.target.closest("li").appendChild(ul_rtr);
-    // console.log(e.target.closest(".reply"));
     replyClone.remove();
   };
 }
@@ -203,223 +203,64 @@ function createEditDelete() {
     modal.querySelector(".modalClose").onclick = closeModal;
   };
 
-
-
   editDelete.querySelector(".icon-edit").onclick = (e) => {
-
-
-    /* 
-    if (textArea.style.display === "none") {
-      // textareaが見えない状態なので、
-      １．見えるようにする＋valueをpタグのtextContent
-      ２．pタグを見えなくする
-      ３．UPDATEボタンも見えるようにする
-    }else {
-      // textareaが見えている状態なので、
-      １．見えなくする
-      ２．pタグを見えるようにする＋p.tagのtextContentをtextareaタグのvalueにする
-      ３．UPDATEボタンも見えないようにする
-
-    }
-    
-    
-    UPDATEボタンがクリックされたら
-        １．p.textContent=textArea.value
-        ２． textareaを見えないようにする
-        ３．pタグを見えるようにする
-        ４．UPDATEボタンも見えないようにする
-    
-    */
-    const existingTextarea = e.target.closest("li").querySelector("textarea");
-
-    // 近くのt.textを取得する
     const p_text = e.target.closest("li").querySelector("p.text");
-    const ta = e.target.closest("li").querySelector("textarea")
-
-
+    const ta = e.target.closest("li").querySelector("textarea");
 
     const existingUdbtn = e.target.closest("li").querySelector(".button");
     if (existingUdbtn) {
       existingUdbtn.remove();
     }
 
+    const update_div = createUpdateButton(ta);
 
-    const editTemplate = document
-      .getElementById("edit-reply-template")
-      .content.cloneNode(true).firstElementChild;
-
-
-    // textコンテンツを取得する
-    const text = p_text.textContent;
-
-
-    //UPDATEボタン作成
-    const updateButton = editTemplate.querySelector(".button");
-    console.log(updateButton);
-
-    const update_div = document.createElement("div");
-    update_div.classList.add("update-button-box");
-    const update_button = document.createElement("button");
-    console.log("update_button", update_button);
-    update_button.classList.add("button");
-    update_button.setAttribute("id", "udbtn");
-    update_button.textContent = "UPDATE";
-    update_div.appendChild(update_button);
-
-    ta.insertAdjacentElement("afterend", update_div);
-    update_div.classList.add("hide")
-    
     if (ta.classList.contains("hide")) {
-      ta.classList.remove("hide")
-      ta.value = p_text.textContent
-      p_text.classList.add("hide")
-      update_div.classList.remove("hide")
-      
-    }else{
-      ta.classList.add("hide")
-      p_text.classList.remove("hide")
+      ta.classList.remove("hide");
+      ta.value = p_text.textContent;
+      p_text.classList.add("hide");
+      update_div.classList.remove("hide");
+    } else {
+      ta.classList.add("hide");
+      p_text.classList.remove("hide");
     }
 
-    // if (ta.style.display === "none") {
-    //   console.log("marron");
-    //   ta.style.display = ""
-    //   ta.value = p_text.textContent;
-  
-    //   p_text.style.display = "none"
-    //   // update_div.style.display = ""
-
-    // } else {
-    //   console.log("haku");
-    //   ta.style.display = "none"
-    //   p_text.style.display = ""
-    //   p_text.textContent = ta.value
-    //   // update_div.style.display = "none"
-
-    // }
-
-        // if (textArea.style.display === "none") {
-    //   // textareaが見えない状態なので、
-    //   １．見えるようにする＋valueをpタグのtextContent
-    //   ２．pタグを見えなくする
-    //   ３．UPDATEボタンも見えるようにする
-    // }else {
-    //   // textareaが見えている状態なので、
-    //   １．見えなくする
-    //   ２．pタグを見えるようにする＋p.tagのtextContentをtextareaタグのvalueにする
-    //   ３．UPDATEボタンも見えないようにする
-
-    // }
-    
-    
-    // UPDATEボタンがクリックされたら
-    //     １．p.textContent=textArea.value
-    //     ２． textareaを見えないようにする
-    //     ３．pタグを見えるようにする
-    //     ４．UPDATEボタンも見えないようにする
-    
-    // */
-
-    // const newText = ta.value;
-    // const newTextBox = document.createElement("p");
-
-    // updateFunctionality(updateButton)
-    const tmp = document.getElementById("udbtn");
-    tmp.onclick = (e) => {
+    update_div.onclick = (e) => {
       if (e.target.parentElement.previousElementSibling.value === "") {
         e.disabled = true;
-  
+
         return;
       }
       const newText = ta.value;
-      // const newTextBox = document.createElement("p");
-      // newTextBox.classList.add("text");
-      // e.target.parentElement.appendChild(newTextBox)
-      console.log("newText", newText);
-      p_text.innerHTML = newText;
-      // ta.replaceWith(p_text);
-      // ta.parentElement.appendChild(p_te)
-      ta.classList.add("hide")
-      p_text.classList.remove("hide")
+      p_text.textContent = newText;
+      ta.classList.add("hide");
+      p_text.classList.remove("hide");
       e.target.closest("li").querySelector(".date").textContent =
         date().textContent;
 
-      update_div.remove()
-      // newText.classList.remove("text");
+      update_div.remove();
     };
-
-
-
-
-    // if (ta.style.display === "none") {
-    //   console.log("marron");
-    //   // p_text.classList.add("hide");
-    //   // ta.classList.remove("hide")
-    //   p_text.style.display = "none";
-    //   ta.style.display = "";
-    //   update_button.style.display = "";
-    //   // existingTa.classList.remove("hide")
-    // } else if (newTextBox) {
-    //   const newTextarea = document.createElement("textarea");
-    //   newTextarea.classList.add("text");
-    //   newTextarea.innerHTML = newText;
-    //   console.log("newTextarea", newTextarea);
-    //   // console.log(e.target.closest("li"));
-    //   e.target.closest("li").querySelector("p").remove();
-    //   // e.target.closest("li").appendChild(newTextarea);
-    // } else {
-    //   console.log("haku");
-    //   // p_text.classList.remove("hide")
-    //   // ta.classList.add("hide")
-    //   p_text.style.display = "";
-    //   ta.style.display = "none";
-    //   update_button.remove();
-    // }
-
-
-
-
-
-
-
-
-
-
   };
 
   return editDelete;
 }
 
-export function processReply(comment, ul_replies, currentUser, li) {
-  const clone = createCommentContent(
-    comment.score,
-    comment.user.username,
-    comment.user.image.webp,
-    comment.createdAt,
-    comment.content,
-    comment.user.username === "juliusomo"
-  );
+function createUpdateButton(ta) {
+  const update_div = document.createElement("div");
+  update_div.classList.add("update-button-box");
+  const update_button = document.createElement("button");
+  update_button.classList.add("button");
+  update_button.setAttribute("id", "udbtn");
+  update_button.textContent = "UPDATE";
+  update_div.appendChild(update_button);
 
-  addScoreFunctionality(clone, comment.score);
+  ta.insertAdjacentElement("afterend", update_div);
+  update_div.classList.add("hide");
 
-
-  const li_reply = document.createElement("li");
-  li_reply.classList.add("reply");
-  li_reply.appendChild(clone);
-
-  ul_replies.appendChild(li_reply);
-  li.appendChild(ul_replies);
-
-  const ta = document.createElement("textarea")
-  ta.classList.add("text")
-  ta.value = li_reply.querySelector("p").textContent
-  ta.classList.add("hide")
-  clone.appendChild(ta)
-  addReplyFunctionality(clone, currentUser, ul_replies);
+  return update_div;
 }
 
 function openModal() {
   const modal = document.getElementById("easyModal");
-
   addEventListener("click", outsideClose);
   modal.style.display = "block";
   return modal;
@@ -436,62 +277,28 @@ function outsideClose(e) {
   }
 }
 
-function createAddComment(src) {
+function currentUserInfoCmt(src) {
   const template = document.getElementById("comment-form");
   const templateClone = template.content.cloneNode(true);
 
   templateClone.querySelector("img.avatars2").src = src;
-  // templateClone.querySelector("textarea") = textarea;
-  // clone.querySelector("button") = button;
-
-  // const send = templateClone.querySelector("button");
-  // addCommentFunctionality(clone.firstElementChild);
 
   return templateClone;
 }
 
-export function addComment(currentUser) {
-  const template = document.getElementById("comment-form");
-  // template.classList.add("add-comment")
-  const templateClone = template.content.cloneNode(true).firstElementChild;
-  console.log(templateClone);
-
-  const clone = createAddComment(currentUser.image.webp);
-  addCommentFunctionality(clone, currentUser, templateClone);
-  document
-    .getElementById("comments")
-    .insertAdjacentElement("afterend", clone.firstElementChild);
-}
-
-function createAddReply(src) {
+function CurrentUserInfoReply(src) {
   const template = document.getElementById("reply-form-template");
-  // template.classList.add("add-comment")
   const clone = template.content.cloneNode(true).firstElementChild;
 
   clone.querySelector("img.avatars2").src = src;
-  // clone.querySelector("textarea") = textarea;
-  // clone.querySelector("button") = button;
 
   return clone;
-}
-
-export function processAddReply(currentUser) {
-  const clone = createAddReply(currentUser.image.webp);
-
-  const template = document.getElementById("comment");
-  template.querySelector(".icon-reply").onclick = () => {
-    template.appendChild(clone);
-  };
-  // document.getElementById("comment").appendChild(clone)
-  addScoreFunctionality(clone, score);
 }
 
 function date() {
   const template = document.getElementById("reply-content-template");
   const clone = template.content.cloneNode(true).firstElementChild;
   const dateBox = clone.querySelector(".date");
-  console.log(dateBox);
-  // date.classList.add("date")
 
   const timeAgo = (date) => {
     const seconds = Math.floor((new Date() - date) / 1000);
--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-import { getFileData,processComment,processReply,addComment,processAddReply } from "./helpers.js";
+import { getFileData, processComment, commentForm } from "./helpers.js";
 
-await getFileData().then(data => {
-
-
-  const currentUser = data.currentUser
-  addComment(currentUser);
+await getFileData().then((data) => {
+  const currentUser = data.currentUser;
+  commentForm(currentUser);
   // processReply(currentUser);
 
-
   for (const comment of data.comments) {
-    processComment(comment,currentUser);
-
+    processComment(comment, currentUser);
   }
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const username = "motosugi";
-function hello(name) {
-  console.log("hello "+ name + username);
-}
-console.log(username); // motosugi ERROR
-// hello('beki') // hello bekimotosugi
